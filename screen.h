437367aaddda31f7a96f601afd421d114d0179c2
Thanh Dat PHAN <ptdat.thanhdatphan@gmail.com>
2019-05-02 22:40:50 +0300

    Update screen.h

--- screen.h
+++ screen.h
@@ -1,6 +1,8 @@
 #define UNICODE
 //constant definitions
 enum COLORS{BLACK=30, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE};
+#define BAR "\u2590"
+#define COL 80
 #define bg(c) (c+10) //macro difinition
 // function declarations
 void clearScreen(void);
